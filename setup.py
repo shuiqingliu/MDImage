b2a5ebc090ba96dcb8d2405d225a2ff9c656810c
qingliu <shuiqingliu14@gmail.com>
2018-02-04 22:25:53 +0800

    Add dependent describe

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='MDImage',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/shuiqingliu/MDImage',
+    license='LGPLv3',
+    author='qingliu',
+    author_email='shuiqingliu14@gmail.com',
+    description='MDimage is a telegram bot that helps you to upload image to qiniu and reply the qiniu outerlink to you.That helps you to write MarkDown File quickly.'
+)
